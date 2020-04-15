38b621598240c065cda05a618ae99689a949cc86
Ale55andra <alessandracoote@gmail.com>
2020-04-15 14:06:07 -0700

    fixed typos

--- Project2/project_2_logistic_regression.r
+++ Project2/project_2_logistic_regression.r
@@ -3,6 +3,8 @@
 library(dplyr)
 library(ISLR)
 library(car) 
+install.packages("pROC")
+library(pROC)
 
 raw_data <- read.csv("~/Documents/STAT05/Project2/diabetes.csv")
 
@@ -26,8 +28,14 @@ table(actual_outcome = test_data$Outcome, predicted_outcome = predicted_probs>0.
 # As predicted probability goes up, the correct false identifications increase, but the alpha value does as well
 # As the predicted probability goes down, the correct true identifications increase, but the beta value does as well
 
+# Plot ROC 
+
+g <- roc(Outcome ~ predicted_probs, data = test_data)
+plot(g)
+
+
 #From your model, what is the increase  in the odds of having  diabetes if your BMI goes up by 1?
-# An increase of 0.0864 Probability (from coefficents table)
+# An increase of e^0.0864 Probability (from coefficents table)
 plot_data <- cbind(test_data, predicted_probs)
 plot(plot_data$predicted_probs, plot_data$BMI, xlab="Predicted Probability",
     ylab="BMI" ,main="BMI vs Predicted Probability of Diabetes")
@@ -35,7 +43,7 @@ lines(lowess(plot_data$predicted_probs,plot_data$BMI), col="blue") # lowess line
 my_model[["coefficients"]] 
 
 # What about if your Glucose level increases  by 1?
-# An increase of 0.0302 (from coefficents table)
+# An increase of e^0.0302 (from coefficents table)
 plot_data <- cbind(test_data, predicted_probs)
 plot(plot_data$predicted_probs, plot_data$Glucose, xlab="Predicted Probability",
      ylab="BMI" , main="Glucose levels vs Predicted Probability of Diabetes")
